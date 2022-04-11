7a7644f61181972a84cf24fed687a286642475af
Dane Madsen <dane_madsen@hotmail.com>
2022-04-11 15:04:50 +1000

    Fixed GUI

---
+++ maze-app/src/GUI.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+/***
+ * Class for managing spring MainGUI
+ */
+
+public class GUI extends JFrame {
+    public GUI() {
+        // Create a new JFrame called mainFrame and enable exit on close
+        this.setTitle("MazeApp");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set mainFrame window size to 800x600 (not working)
+        this.setSize(800, 600);
+
+        // Set mainFrame visibility to true
+        this.setVisible(true);
+        this.pack();
+        this.setLocationRelativeTo(null);
+    }
+
+
+}
