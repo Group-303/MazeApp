7f1f4320e6bd7f49cda6fa33adc576d0580efc45
Cameron Turley <n11000058@qut.edu.au>
2022-05-12 15:59:06 +1000

    Added a number of JavaDoc comments to methods

--- maze-app/src/IMenu.java
+++ maze-app/src/IMenu.java
@@ -1,4 +1,11 @@
 public interface IMenu {
+    /***
+     * Method containing actions performed when menu is opened
+     */
     void openMenu();
+
+    /***
+     * Method containing actions performed when menu is closed
+     */
     void closeMenu();
 }
